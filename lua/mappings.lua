4b250aa8f75f78b7600a3b7baab9bc8ec66b05ee
leosalgado <leonardosalgado@protonmail.com>
2025-04-15 01:37:24 -0300

    Add copy to system clipboard

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+map("v", "<leader>y", '"+y', { desc = "Copy selection to system clipboard" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
