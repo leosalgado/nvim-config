48a9f03840034999982f2f684e38bd9011d213fe
leosalgado <leonardosalgado@protonmail.com>
2025-10-26 04:52:20 -0300

    Fix lspconfig

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,9 +4,11 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "v" }, ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "v" }, ":", ";", { desc = "Find next occurrence" })
 map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 map("v", "<leader>y", '"+y', { desc = "Copy selection to system clipboard" })
 map("n", "<leader>lc", ":VimtexClean<CR>", { desc = "Clean LaTeX files" })
+map("n", "<leader>gi", ":VGit buffer_diff_preview<CR>", { desc = "View git diff" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,11 +17,33 @@ return {
     end,
   },
 
+  {
+    "tanvirtin/vgit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    -- Lazy loading on 'VimEnter' event is necessary.
+    event = "VimEnter",
+    config = function()
+      require("vgit").setup {
+        settings = {
+          live_blame = {
+            enabled = false,
+          },
+          scene = {
+            diff_preference = "split",
+            keymaps = {
+              quit = "q",
+            },
+          },
+        },
+      }
+    end,
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
-      vim.lsp.config = "configs.lspconfig"
+      require "configs.lspconfig"
     end,
   },
 
