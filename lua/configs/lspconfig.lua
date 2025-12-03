a22361ba2ca102d4e3042f4f3a97916b38f5f67f
leosalgado <leonardosalgado@protonmail.com>
2025-12-02 23:47:20 -0300

    Update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "gopls", "pyright", "ts_ls", "rust_analyzer", "hls", "texlab" }
+local servers = { "html", "cssls", "clangd", "gopls", "pyright", "ts_ls", "hls", "texlab" }
 
 vim.lsp.enable(servers)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,6 +47,12 @@ return {
   },
 
   {
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+
+  {
     "stevearc/conform.nvim",
     event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
